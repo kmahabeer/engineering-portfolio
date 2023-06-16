9e6d7e16537429ba294136143d93983a025991bd
Kevin Mahabeer <11927611+kmahabeer@users.noreply.github.com>
2023-06-16 01:15:30 -0400

    feat: add navbar [c198_5b7a_0012-T0-124]

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const Navbar = () => {
+	return (
+		<nav className='w-full bg-black sticky top-0 left-0 right-0 z-10'>
+			<div className='justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8'>
+				<div>
+					<div className='flex-1 items-center justify-between py-3 md:py-5 md:block '>
+						<Link href={'/'} className='btn btn-ghost normal-case text-2xl'>
+							Kevin Mahabeer
+						</Link>
+					</div>
+				</div>
+
+				<div>
+					<ul className='flex-none menu menu-horizontal py-3 md:py-5 text-lg'>
+						<li>
+							<Link href={'/projects'}>Projects</Link>
+						</li>
+						<li>
+							<Link href={'/resume'}>Resume</Link>
+						</li>
+						<li>
+							<Link href={'/about'}>About</Link>
+						</li>
+						<li>
+							<Link href={'/contact'}>Contact</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
