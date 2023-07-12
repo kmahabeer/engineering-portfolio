1eec510c96d80909d78d2b7a1a658e7454e81dbe
Kevin Mahabeer <11927611+kmahabeer@users.noreply.github.com>
2023-07-12 00:13:04 -0400

    chore: configure rich text formatting [c198_5b7a_0012-T0-246]

--- src/components/RichText.tsx
+++ src/components/RichText.tsx
@@ -1,15 +1,70 @@
-import { Document as RichTextDocument } from '@contentful/rich-text-types';
+import {
+	BLOCKS,
+	MARKS,
+	INLINES,
+	Document as RichTextDocument,
+} from '@contentful/rich-text-types';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Link from 'next/link';
 
 type RichTextProps = {
 	document: RichTextDocument | undefined | null;
+};
+
+const options = {
+	renderMark: {
+		[MARKS.CODE]: (code: any) => {
+			return (
+				<pre>
+					<code>{code}</code>
+				</pre>
+			);
+		},
+	},
+	renderNode: {
+		[BLOCKS.PARAGRAPH]: (node: any, children: any) => {
+			if (
+				node.content.find((item: any) =>
+					item.marks?.find((mark: { type: string }) => mark.type === 'code')
+				)
+			) {
+				return (
+					<div>
+						<pre>
+							<code>{children}</code>
+						</pre>
+					</div>
+				);
+			}
+			return <p>{children}</p>;
+		},
+		[INLINES.ENTRY_HYPERLINK]: (node: any) => {
+			if (node.data.target.sys.contentType.sys.id === 'engineeringProjects') {
+				return (
+					<Link href={`/projects/${node.data.target.fields.slug}`}>
+						{node.data.target.fields.title}
+					</Link>
+				);
+			}
+		},
+		[INLINES.HYPERLINK]: (node: any) => {
+			const text = node.content.find(
+				(item: any) => item.nodeType === 'text'
+			)?.value;
+			return (
+				<a href={node.data.uri} target='_blank' rel='noopener noreferrer'>
+					{text}
+				</a>
+			);
+		},
+	},
 };
 
 function RichText({ document }: RichTextProps) {
 	if (!document) {
 		return null;
 	}
-	return <>{documentToReactComponents(document)}</>;
+	return <>{documentToReactComponents(document, options)}</>;
 }
 
 export default RichText;
