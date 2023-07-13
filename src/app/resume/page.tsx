035f7c259a3b03355779ed93412856501f88ff94
Kevin Mahabeer <11927611+kmahabeer@users.noreply.github.com>
2023-07-12 20:14:21 -0400

    chore: add direct link to resume in navbar (temporarily) [c198_5b7a_0012-T0-256]

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
 const page = () => {
-  return (
-		<div>
-			<h1>Here is the resume page.</h1>
+	const { RESUME_TMP } = process.env;
+	return (
+		<div className='flex justify-center min-h-screen px-4 mx-auto lg:max-w-5xl overflow-y-hidden'>
+			<section className='flex lg:px-20 py-6 px-5 mb-16'>
+				<object
+					data={RESUME_TMP}
+					type='application/pdf'
+					width={900}
+					height={800}
+				>
+					<iframe src={RESUME_TMP} width={750} height={800}></iframe>
+				</object>
+			</section>
 		</div>
-  )
-}
+	);
+};
 
-export default page
+export default page;
